2e53802c0e18f857af0a509112bfd1fe076a6251
Gugnu <lochammehtab@gmail.com>
2022-02-19 18:32:08 -0500

    display user's friends

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -15,8 +16,25 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+/* When we need to use a specific function that a library provides us, we might not need to use every parameter for that function. 
+Often, we can't omit an unused parameter, because the function is looking for these parameters in a specific order.
+In this case, we don't need the first parameter offered by setContext(), which stores the current request object in
+case this function is running after we've initiated a request.
+Because we're not using the first parameter, but we still need to access the second one,
+we can use an underscore _ to serve as a placeholder for the first parameter. */
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
